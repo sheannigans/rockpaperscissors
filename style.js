c69e9d2bbc98dc11e1bd411349c2219cc58bb6ce
sheannigans <sheannigans@Xubuntu.myguest.virtualbox.org>
2023-10-05 15:57:23 -0700

    feat: define variables for player computer and results and create buttons

--- style.js
+++ style.js
@@ -1,11 +1,17 @@
-var computerChoice =Math.random ();
-function getComputerChoice () {
-    if (computerChoice < 0.34) {
-        console.log("rock");
-    } else if (computerChoice <= 0.6) {
-        console.log("paper");
-    } else {
-        console.log("scissors");
-    } 
-    
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener ("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+}));
+
+function computerTurn () {
+    const randNum = Math.floor(Math.random() * 3) +1;
 }
