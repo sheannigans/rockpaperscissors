6b53194656b906526a484df3362c5e0822a3cede
sheannigans <sheannigans@Xubuntu.myguest.virtualbox.org>
2023-10-05 16:06:28 -0700

    feat: add functional text for player computer and results

--- style.js
+++ style.js
@@ -10,8 +10,23 @@ choiceBtns.forEach(button => button.addEventListener ("click", () => {
 
     player = button.textContent;
     computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${player}`;
+    resultsText.textContent = checkWinner ();
 }));
 
 function computerTurn () {
     const randNum = Math.floor(Math.random() * 3) +1;
+
+    switch(randNum){
+        case 1:
+            computer = "Rock";
+            break;
+        case 2:
+            computer = "Paper";
+            break;
+        case 3:
+            computer = "Scissors";
+            break;    
+    }
 }
